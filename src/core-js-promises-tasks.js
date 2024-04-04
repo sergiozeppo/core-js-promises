34a93212f35d663134aeb28fc7da86a2541c8530
sergiozeppo <sp1tfire_90@mail.ru>
2024-04-04 11:20:33 +0500

    feat: solve queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -142,8 +142,12 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return promises.reduce(async (acc, promise) => {
+    return acc.then(async (result) => {
+      return promise.then((value) => result + value.toString());
+    });
+  }, Promise.resolve(''));
 }
 
 module.exports = {
